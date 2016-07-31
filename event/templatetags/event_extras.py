d1b5e91b28b01fcfc5d9875215983e0430bab085
Eleyine Zarour <eleyine@gmail.com>
2016-07-30 23:56:54 -0400

    fix bootstrap columns django tag
    I think it was a spacing issue

--- event/templatetags/event_extras.py
+++ event/templatetags/event_extras.py
@@ -2,20 +2,24 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='boostrap_columns')
-def boostrap_columns(count):
+import calendar
+@register.filter
+def month_name(month_number):
+    return calendar.month_name[month_number][:3]
+    
+@register.filter
+def bcol(count):
     """Returns Twitter bootstrap column css classes for a responsive design"""
-
     if count == 1:
-    	css_classes = 'col-lg-12 col-md-12'
+      css_classes = 'col-lg-12 col-md-12'
     elif count == 2:
-    	css_classes = 'col-lg-6 col-md-12'
-	elif count == 3:
-    	css_classes = 'col-lg-4 col-md-6'
+      css_classes = 'col-lg-6 col-md-12'
+    elif count == 3:
+      css_classes = 'col-lg-4 col-md-6'
     elif count == 4:
-    	css_classes = 'col-lg-3 col-md-6'
+      css_classes = 'col-lg-3 col-md-6'
     elif count == 5:
-    	css_classes = 'col-lg-4 col-md-6'
+      css_classes = 'col-lg-4 col-md-6'
     else:
-    	css_classes = 'col-lg-3 col-md-6'
+      css_classes = 'col-lg-3 col-md-6'
     return css_classes
